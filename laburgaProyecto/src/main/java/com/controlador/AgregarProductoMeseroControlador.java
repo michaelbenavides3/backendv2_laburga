dbbef197cbc1c8282d612e7e3cb68a32f3695190
michaelbenavides <michaelfb803@gmail.com>
2026-07-09 09:00:27 -0500

    comentarios

--- laburgaProyecto/src/main/java/com/controlador/AgregarProductoMeseroControlador.java
+++ laburgaProyecto/src/main/java/com/controlador/AgregarProductoMeseroControlador.java
@@ -1,6 +1,6 @@
 /*
     
-    CONTROLADOR AGREGAR PRODUCTO MESERO
+    CONTROLADOR AGREGAR PRODUCTO MESERO NUEVO PEDIDO
     
     RESPONSABILIDAD:
     
@@ -65,7 +65,10 @@ public class AgregarProductoMeseroControlador extends HttpServlet {
         2. CREAR OBJETOS DAO
         
          */
-
+        /*
+        se instancia la clase productosdao e imagendao, 
+        
+        */
         ProductosDao productosDao     = new ProductosDao();
         ProductoImagenDao imagenDao   = new ProductoImagenDao();
         
@@ -73,6 +76,9 @@ public class AgregarProductoMeseroControlador extends HttpServlet {
         
         3. OBTENER TODOS LOS PRODUCTOS DISPONIBLES Solo trae productos activos.
         
+         */
+         /*
+         productosDao--> es el resultado de la instacia del objeto dao
          */
 
         List<Productos> productosDisponibles = productosDao.obtenerProductosDisponibles();
@@ -95,9 +101,26 @@ public class AgregarProductoMeseroControlador extends HttpServlet {
          */
 
         // Agrupamos por categoría
+        /*
+        SE ALMACENAN DE FORMA CATEGORIA Y VALOR (LinkedHashMap) --> MANTIENE EL ORDEN EN QUE SE VA INSERTANDO LAS CATEGORIAS
+        
+        */
         Map<String, List<Productos>> menuPorCategoria = new LinkedHashMap<>();
+        /*
+        CON EL FOR UTILIZAMOS PARA RECORRES TODOS LOS PRODUCTOS (P), SON LOS PRODUCTO QUE SE ESTAN ANALIZANDO
+        P es la variable temporal que va aumentando cada vuelta (productosDisponibles) donde estan todos los productos es la variable de la lista donde se obtuvieron los productos disponibles
+        */
         for (Productos p : productosDisponibles) {
+            /*
+            cat es una varibale que se llama categoria, donde guarda la respuesta (hamburguesas, otros, sopas)
+            p que es el productos encontrar lo asigna dentro de cat, 
+            */
             String cat = p.getCategoriaProducto();
+            /*
+            menuporcategoria, es el mapa donde se agruoan las categrias o el diccionario, 
+            (computeIfAbsent)--> se obtiene la lista de productos guardados dentro de la categoria
+            (cat, k -> new java.util.ArrayList<>()).add(p)--> si no existe la categoria creela una lista  nueva, y add, agregaa el producto nuevo dentro de la categoria,
+            */
             menuPorCategoria.computeIfAbsent(cat, k -> new java.util.ArrayList<>()).add(p);
         }
         
